62e4f2e37271eabefce0394acb0e5b48a3b90233
joaorihan <joao.rihan@gmail.com>
2024-08-12 19:55:36 -0300

    Initial commit

---
+++ ExemploSplash/src/main/java/com/joaorihan/exemplosplash/ExemploSplash.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.joaorihan.exemplosplash;
+
+import com.joaorihan.exemplosplash.view.Splash_GUI;
+
+/**
+ *
+ * @author labsfiap
+ */
+public class ExemploSplash {
+
+    public static void main(String[] args) {
+        new Splash_GUI();
+    }
+}
